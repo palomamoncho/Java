9cd653fa80773c878dc7a3488b3cc83d7b136311
palomamoncho <128047562+palomamoncho@users.noreply.github.com>
2025-02-10 20:05:46 +0100

    Delete Main3.java

--- Main3.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int numeroEntero = 100;
-        double numeroDecimal = numeroEntero;
-        System.out.println("Conversión implícita: int (" + numeroEntero + ") -> double (" + numeroDecimal + ")");
-    }
-}
