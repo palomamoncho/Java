da1f6ccdc7bb6ffd268b4602ce05c8c4af15021e
palomamoncho <128047562+palomamoncho@users.noreply.github.com>
2025-02-10 20:04:55 +0100

    commit

--- proyecto001/Main2.java
+++ proyecto001/Main2.java
@@ -2,7 +2,7 @@ public class Main2 {
     public static void main(String[] args) {
         int maxValue = Integer.MAX_VALUE;
         System.out.println("Valor máximo de un int: " + maxValue);
-        // Sumamos 1 al valor máximo. lo que causa un overflow
+        // Sumamos 1 al valor máximo, lo que causa un overflow
         int overflowedValue = maxValue + 1;
 
         System.out.println("Valor después del overflow: " + overflowedValue);
