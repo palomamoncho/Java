1393c99e49317866dcf0fdc4f1c79c91d37e8d74
palomamoncho <128047562+palomamoncho@users.noreply.github.com>
2025-02-11 18:24:13 +0100

    ok

--- proyecto001/Main10.java
+++ proyecto001/Main10.java
@@ -1,5 +1,5 @@
 public class Main10 {
-    public static void main(Strin[] args) {
+    public static void main(String[] args) {
         String textoNumero = "123";
         int numeroConvertido = Integer.parseInt(textoNumero); //String -> int
         System.out.printl("Conversión de String a int: " + numeroConvertido);
