c5f2646930a697f795d0ef5945e5bbd3cce18681
palomamoncho <128047562+palomamoncho@users.noreply.github.com>
2025-02-14 18:25:29 +0100

    scannerdiamio

---
+++ proyecto001/scannermiodia.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class scannermiodia {
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in); // Supongamos que 3 representa el miércoles
+System.out.print("introduce un dia de la semana:");
+int dia = scanner.nextInt();
+switch (dia) {
+case 1:
+System.out.println("Lunes");
+break;
+case 2:
+System.out.println("Martes");
+break;
+case 3:
+System.out.println("Miércoles");
+break;
+case 4:
+System.out.println("Jueves");
+break;
+case 5:
+System.out.println("Viernes");
+break;
+case 6:
+System.out.println("Sábado");
+break;
+case 7:
+System.out.println("Domingo");
+break;
+default:
+System.out.println("Día no válido");
+}
+}
+}
