6c85ff9f9ff81de9766f7b96731104eeab2f7675
palomamoncho <128047562+palomamoncho@users.noreply.github.com>
2025-02-14 17:56:18 +0100

    case

---
+++ proyecto001/case.java
@@ -0,0 +1,30 @@
+public class case {
+public static void main(String[] args) {
+int dia = 3; // Supongamos que 3 representa el miércoles
+switch (dia) {
+case 1:
+System.out.println("Lunes");
+break;
+case 2:
+System.out.println("Martes");
+break;
+case 3:
+System.out.println("Miércoles");
+break;
+case 4:
+System.out.println("Jueves");
+break;
+case 5:
+System.out.println("Viernes");
+break;
+case 6:
+System.out.println("Sábado");
+break;
+case 7:
+System.out.println("Domingo");
+break;
+default:
+System.out.println("Día no válido");
+}
+}
+}
