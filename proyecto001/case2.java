7599990f8c049a5406ecf03ae3b7b27a03bcc032
palomamoncho <128047562+palomamoncho@users.noreply.github.com>
2025-02-17 10:37:44 +0100

    ok

---
+++ proyecto001/case2.java
@@ -0,0 +1,15 @@
+public class case {
+    public static void main(String[] args) {
+        int dia = 3;
+        switch (dia) {
+            case 1 -> Syste.out.println("Lunes");
+            case 2 -> Syste.out.println("Martes");
+            case 3 -> Syste.out.println("Miércoles");
+            case 4 -> Syste.out.println("Jueves");
+            case 5 -> Syste.out.println("Viernes");
+            case 6 -> Syste.out.println("Sábado");
+            case 7 -> Syste.out.println("Domingo");
+            default -> System.out.println("Día no válido");
+        }
+    }
+}
---
+++ proyecto001/for.java
@@ -0,0 +1,5 @@
+public class for {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo 1: Números del 1 al 5");
+    }
+}
