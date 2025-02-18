3545427d4832244324a0477a6a13ead29dad7b6e
palomamoncho <128047562+palomamoncho@users.noreply.github.com>
2025-02-18 19:49:31 +0100

    ejemplofunciones

---
+++ proyecto001/ejemplofunciones.java
@@ -0,0 +1,9 @@
+public class ejemplofunciones {
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int resultado = sumar(5, 3);
+        System.out.println("La suma es: " + resultado);
+    }
+}
