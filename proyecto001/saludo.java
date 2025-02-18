75e5d1dc92d29843887d8d81c1973bf3d0f9ff74
palomamoncho <128047562+palomamoncho@users.noreply.github.com>
2025-02-18 19:52:33 +0100

    saludofuncion

---
+++ proyecto001/saludo.java
@@ -0,0 +1,8 @@
+public class saludo {
+    public static void saludar() {
+        System.out.println("¡Hola, bienvenido a Java!");
+    }
+    public static void main(String[] args) {
+        saludar();
+    }
+}
