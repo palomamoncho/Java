f8796ad6993838e0b820ebef13907d4ae490b390
palomamoncho <128047562+palomamoncho@users.noreply.github.com>
2025-02-18 19:57:50 +0100

    calculadora error

---
+++ proyecto001/calculadora.java
@@ -0,0 +1,9 @@
+public class calculadora {
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int resultado = sumar(5, 3);
+        System.out.println("La suma es: " + resultado);
+    }
+}
