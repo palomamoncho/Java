f0c77803a37cdca0196a9422912a728f3fded784
palomamoncho <128047562+palomamoncho@users.noreply.github.com>
2025-02-21 09:25:46 +0100

    calcularprecio

---
+++ proyecto001/calcularprecio.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CalculadoraPrecio {
+
+    static final double IVA = 0.21;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce el precio del producto: ");
+        double precio = scanner.nextDouble();
+
+        System.out.print("Introduce el descuento en porcentaje (ej. 20 para un 20%): ");
+        double descuento = scanner.nextDouble();
+        double precioFinal = calcularPrecioFinal(precio, descuento);
+
+        System.out.println("El precio final después de aplicar el descuento y el IVA es: " + precioFinal);
+    }
+
+    public static double calcularPrecioFinal(double precio, double descuento) {
+        double precioConDescuento = precio - (precio * descuento / 100);
+
+        double precioConIVA = precioConDescuento * (1 + IVA);
+
+        return precioConIVA;
+    }
+}
