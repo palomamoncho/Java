fa8797dc9e1bd76a74bf1ded15f7eb9176a3dc85
palomamoncho <128047562+palomamoncho@users.noreply.github.com>
2025-03-03 17:38:16 +0100

    okey

---
+++ proyecto001/animal.java
@@ -0,0 +1,14 @@
+class Animal {
+    String nombre;
+    String especie;
+    Static String Emitirsonido = "emitirsonido";
+    voiod emitirsonido() {
+        System.out.println("El" + especie + "llamado" + nombre + "hace un sonido.");
+    }
+}
+public class {
+    public Static void animal(String[] args) {
+        Animal animal = new Animal();
+        System.out.println(Animal.Emitirsonido);
+    }
+}
---
+++ proyecto001/persona.java
@@ -0,0 +1,17 @@
+class Persona {
+    String nombre;
+    int edad;
+    static String nombrePersona = "Persona";
+    void presentarse() {
+        System.out.println("Tú nombre es " + nombre + " y tu edad es:" + edad);
+    }
+}
+public class persona {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        System.out.println(Persona.nombrePersona);
+        persona.nombre = "Paloma";
+        persona.edad = 22;
+        persona.presentarse();
+    }
+}
