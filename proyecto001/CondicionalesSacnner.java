97f4ff252fb23f053c8ad74d967b0d30b8cdee97
palomamoncho <128047562+palomamoncho@users.noreply.github.com>
2025-03-10 12:20:56 +0100

    s

--- proyecto001/CondicionalesSacnner.java
+++ proyecto001/CondicionalesSacnner.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class scanner1 {
+public class CondicionalesScanner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Introduce tu edad: ");
