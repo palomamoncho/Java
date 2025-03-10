7c27e7de779ebd9fa3e446b3d43f4852e95726ae
palomamoncho <128047562+palomamoncho@users.noreply.github.com>
2025-03-10 12:11:47 +0100

    s

--- proyecto001/BucleForNumeros.java
+++ proyecto001/BucleForNumeros.java
@@ -1,4 +1,4 @@
-public class for {
+public class BucleForNumeros {
     public static void main(String[] args) {
         System.out.println("Ejemplo 1: Números del 1 al 5");
         for (int i = 1; i <= 5; i++) {
